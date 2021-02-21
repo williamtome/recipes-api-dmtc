bb5cad7a4d2ff2f9c809154a8911f112c2349f3c
William Tomé <william.tomenh@gmail.com>
2021-02-21 17:55:02 -0300

    Get the recipes based in endpoint of RecipePuppy site.

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -15,9 +15,37 @@ class RecipeController extends Controller
         if ($request->filled('i') == false) {
             return response()->json("Please, add url parameter 'i' and type the ingredients.");
         }
+
         $this->params = $request->input('i');
         $this->ingredients = explode(',', $this->params);
-        return response()->json($this->ingredients);
+
+        if (count($this->ingredients) > 0 && count($this->ingredients) < 4) {
+            $headers = [
+                'Accept: application/json',
+                'Content-Type: application/json'
+            ];
+
+            define('RECIPE_PUPPY_API', "http://www.recipepuppy.com/api/?i=");
+            $url = $this->getFormatURL(RECIPE_PUPPY_API, $this->ingredients);
+
+            $configs = [
+                CURLOPT_URL => $url,
+                CURLOPT_HEADER => false,
+                CURLOPT_HTTPHEADER => $headers,
+                CURLOPT_RETURNTRANSFER => true
+            ];
+
+            try {
+                $ch = curl_init();
+                curl_setopt_array($ch, $configs);
+                $results = curl_exec($ch);
+                curl_close($ch);
+                return response()->json($results);
+
+            } catch (\Exception $exception) {
+                throw $exception;
+            }
+        }
     }
 
     private function getFormatURL($url, $params): string
