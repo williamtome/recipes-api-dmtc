2acd2d8cbde3c97533aedc355439be9ab019cb00
William Tomé <william.tomenh@gmail.com>
2021-02-21 16:47:00 -0300

    Return the params catch in url.

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -6,8 +6,14 @@ use Illuminate\Http\Request;
 
 class RecipeController extends Controller
 {
+    protected $params;
+
     public function index(Request $request)
     {
-
+        if ($request->input('i') === false) {
+            return response()->json(['data' => ['message' => 'The parameter is incorrect.']], ['status' => 400]);
+        }
+        $this->params = $request->input('i');
+        return $this->params;
     }
 }
