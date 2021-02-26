5b53548b5e6cc94638c9d39ba8519f557db81055
William Tomé <william.tomenh@gmail.com>
2021-02-25 21:54:01 -0300

    Fix

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function (\Illuminate\Http\Request $request) {
-//    return view('welcome');
-    return $request->input('i');
+    return view('welcome');
 });
