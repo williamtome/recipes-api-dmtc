e2d9edd50e7ed0aaefc9df82331db99810c1364c
William Tomé <william.tomenh@gmail.com>
2021-02-27 16:28:10 -0300

    Routes methods get and post.

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/recipes/', [RecipeController::class, 'index']);
+Route::match(['get', 'post'], '/recipes/', [RecipeController::class, 'index']);
